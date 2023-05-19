964b4f00b2166cff68eed8c03041eb3155686632
ShahafS12 <104262302+ShahafS12@users.noreply.github.com>
2023-05-19 12:52:41 +0300

    module changes

--- stepper-console/src/main/Main.java
+++ stepper-console/src/main/Main.java
@@ -11,7 +11,7 @@ public class Main
     public static void main(String[] args) throws IOException, ParserConfigurationException, SAXException
     {
         Menu menu = new Menu();
-        while (true)
+        while (true)//ין
             menu.showMenu();
     }
 }
